e40831174f929a193e2dd3a28b9e0e28dc768695
fredpiech <frederik.piech@gmail.com>
2014-10-28 13:57:47 +0000

    Rotational Matrix implemented along with baseline - UV space mapping further mapped onto Fourier Space. Samples all of Fourier Space. We are happy.

--- VLAArray/VLA.py
+++ VLAArray/VLA.py
@@ -5,7 +5,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 dtheta = 15./(60.**2)   #these will be given by 21cm later on
-theta = 4. #gives 960 points
+theta = 8. #gives 960 points
 size = int(theta/dtheta)
 
 # specify circle parameters: centre ij and radius
@@ -47,33 +47,40 @@ lenpos=len(positions)
 
 
 lda=10
-scaling = 0.5*size*dtheta
 
+
+#Set up dx dy dz vector - this is the baseline vector for
 for i in range(lenpos):
     for j in range(lenpos):
         if i != j:
-            dx.append(ci + int(((float(positions[i][0])-float(positions[j][0]))/(lda))*scaling))  #doesn't take into account rotation of earth or angle of object
-            dy.append(cj + int(((float(positions[i][1])-float(positions[j][1]))/(lda))*scaling))
-            dz.append(((float(positions[i][2])-float(positions[j][2]))/(lda))*scaling)  #do we need the lda or scaling!!!!!
+            dx.append(((float(positions[i][0])-float(positions[j][0]))/(lda)))  #doesn't take into account rotation of earth or angle of object
+            dy.append(((float(positions[i][1])-float(positions[j][1]))/(lda)))
+            dz.append(((float(positions[i][2])-float(positions[j][2]))/(lda)))  #do we need the lda or scaling!!!!!
 
-print len(dx)
-print len(dy)
-print len(dz)
 #float(positions[i][0]) = x coord of ith element
 #float(positions[i][1]) = y coord of ith element
 plt.scatter(dx, dy)
 plt.show()
 
 #this is putting this into uv
-H = 0
-delta = 30
+H = 0.
+dH = 1. / (60.* 24.) * np.pi
+integrationtime = 60
+delta = 30./180. * np.pi
 lenbase = len(dx)
-uvplane = np.zeros((3, lenbase))
+uvplane = np.zeros((3,integrationtime*lenbase ))
 
-for i in range(lenbase):
-    uvplane[0][i] = np.sin(H)*dx[i] + np.cos(H)*dy[i]
-    uvplane[1][i] = -np.sin(delta)*np.cos(H)*dx[i] + np.sin(delta)*np.sin(H)*dy[i] + np.cos(delta)*dz[i]
-    uvplane[2][i] = np.cos(delta)*np.cos(H)*dx[i] - np.cos(delta)*np.sin(H)*dy[i] + np.sin(delta)*dz[i]
+
+scaling = 1./(0.5*size*dtheta)
+
+#This measures the UV plane AND maps it onto our fourier plane
+for t in range (integrationtime):
+    for i in range(lenbase):
+        uvplane[0][i + t*lenbase] = int(ci + scaling*(np.sin(H)*dx[i] + np.cos(H)*dy[i])) # have UV matrix with enough space to get 24 hours of integration
+        #t*lenbase makes sure we dont overwrite previous hours of integration
+        uvplane[1][i + t*lenbase] = int(cj + scaling*(-np.sin(delta)*np.cos(H)*dx[i] + np.sin(delta)*np.sin(H)*dy[i] + np.cos(delta)*dz[i]))
+        uvplane[2][i + t*lenbase] = np.cos(delta)*np.cos(H)*dx[i] - np.cos(delta)*np.sin(H)*dy[i] + np.sin(delta)*dz[i]
+    H += dH
 
 plt.scatter(uvplane[:][0], uvplane[:][1])
 plt.show()
@@ -82,19 +89,19 @@ plt.show()
 #create the measured array
 image = np.zeros((2*ci,2*cj),'complex')
 
-countvar = 0
+countvar = 0.
 #this uses baseline positions to sample - dx/dy[i] gives baseline positions, which then sample the fourier space zhat
-for i in range(len(dx)):
-    if dx[i] < size and dy[i] < size and dx[i] > 0 and dy[i] > 0:   #this is to stop crashing
-        image[dx[i], dy[i]] = zhat[dx[i], dy[i]]
+for i in range(len(uvplane[0])):
+    if uvplane[0][i] < size and uvplane[1][i] < size and uvplane[0][i] > 0 and uvplane[1][i] > 0:   #this is to stop crashing
+        image[uvplane[0][i], uvplane[1][i]] = zhat[uvplane[0][i], uvplane[1][i]]
     else:
-        print ("error, image out of zhat bounds, baseline is", dx[i] , dy[i])
-        countvar += 1
+        #print ("error, image out of zhat bounds, baseline is", uvplane[0][i] , uvplane[1][i])
+        countvar += 1.
 
-print ("percentage of baselines ignored", countvar/(100.*len(dx)))
+print ("percentage of baselines ignored", (100*countvar/(len(uvplane[0]))))
 
 #shows sample fourier image
-image2 = abs(image)
+image2 = np.log(abs(image)+1)
 image2 = plt.imshow(image2, cmap='hot')
 
 
